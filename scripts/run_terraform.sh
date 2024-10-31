5a5555b6a1f1f2280fbbb312873271de1d7a4a7e
hamadm <hamadm@Mac-mini-h.local>
2024-10-31 15:46:59 -0700

    fixed tf run script

--- scripts/run_terraform.sh
+++ scripts/run_terraform.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Initialize Terraform
+cd /abridge-test/terraform
 terraform init
 
 # Run the appropriate Terraform command
 if [ "$COMMAND" = "apply" ]; then
   terraform apply -var-file="$TFVARS_PATH" -auto-approve
 else
-  terraform plan -var-file="$TFVARS_PATH" -out=/var/log/terraform_output/terraform_plan.tfplan
+  terraform plan -var-file="$TFVARS_PATH" -out=/var/log/terraform_plan.tfplan
 fi
 
 # Keep the container running
